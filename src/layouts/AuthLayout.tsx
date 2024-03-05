688fc6c3b07b42cc279eefca59c00656070ef75f
mrx7355608 <mrx7355608@gmail.com>
2024-03-05 18:48:42 +0500

    added route protection

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Container } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useAuthContext } from "../context/auth";
 
 export default function AuthLayout() {
+    const { user } = useAuthContext();
+    const navTo = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            navTo("/");
+        }
+    }, [user, navTo]);
+
     return (
         <Container
             minH="100vh"
