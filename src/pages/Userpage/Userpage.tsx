022f653df1a5391124b1d270586e92af7b3cda81
mrx7355608 <mrx7355608@gmail.com>
2024-03-23 17:44:13 +0500

    completed articles list on user page

--- src/pages/Userpage/Userpage.tsx
+++ src/pages/Userpage/Userpage.tsx
@@ -6,15 +6,15 @@ export default function Userpage() {
         <Box display={"flex"}>
             {/* main content */}
             <Box w="70vw" h="100vh">
-                <Tabs mt="12" p="12" px="16">
-                    <TabList w="full" overflowX="scroll">
-                        <Tab fontSize="sm">Programming</Tab>
-                        <Tab fontSize="sm">Web development</Tab>
-                        <Tab fontSize="sm">Software Engineering</Tab>
-                        <Tab fontSize="sm">Self Improvement</Tab>
-                        <Tab fontSize="sm">Artificial Intelligence</Tab>
-                        <Tab fontSize="sm">Blockchain</Tab>
-                        <Tab fontSize="sm">Web 3 Programming</Tab>
+                <Tabs size={"sm"} mt="12" p="12" px="16" whiteSpace={"nowrap"}>
+                    <TabList overflowY="hidden" height={"full"}>
+                        <Tab py="2">Programming</Tab>
+                        <Tab py="2">Web development</Tab>
+                        <Tab py="2">Software Engineering</Tab>
+                        <Tab py="2">Self Improvement</Tab>
+                        <Tab py="2">Artificial Intelligence</Tab>
+                        <Tab py="2">Blockchain</Tab>
+                        <Tab py="2">Web 3 Programming</Tab>
                     </TabList>
 
                     <TabPanels>
