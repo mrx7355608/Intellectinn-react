c516d961d5382da5f1453e180fc448091faaa1e0
mrx7355608 <mrx7355608@gmail.com>
2024-03-23 17:30:30 +0500

    fixed whiteSpace wrap issue

--- src/components/Articles/TitleAndSummary.tsx
+++ src/components/Articles/TitleAndSummary.tsx
@@ -2,29 +2,23 @@ import { Heading, Box, Text, Image } from "@chakra-ui/react";
 
 export default function TitleAndSummary() {
     return (
-        <Box py="2">
-            <Box
-                display="flex"
-                alignItems="start"
-                justifyContent="space-between"
-                mt="1"
-            >
-                <Box>
-                    <Heading fontSize="xl">My fisrt blog title</Heading>
-                    <Text mt="2">
-                        This is an article's summary. This is an article's
-                        summary. This is an article's summary. This is an
-                        summary.
-                    </Text>
-                </Box>
-                <Image
-                    src="/hero.jpg"
-                    alt="thumbnail"
-                    w="150px"
-                    h="full"
-                    ml="7"
-                />
+        <Box
+            display="flex"
+            alignItems="start"
+            justifyContent="space-between"
+            mt="1"
+            w="full"
+        >
+            <Box w="full" mr="7">
+                <Heading fontSize="xl" w="full">
+                    My fisrt blog title
+                </Heading>
+                <Text mt="2" w="full" whiteSpace={"wrap"}>
+                    This is an article's summary. This is an article's summary.
+                    This is an article's summary. This is an summary.
+                </Text>
             </Box>
+            <Image src="/hero.jpg" alt="thumbnail" w="150px" h="full" />
         </Box>
     );
 }
