0f006fe3ea68c5efc800a666c7942999fde55b08
mrx7355608 <mrx7355608@gmail.com>
2024-03-24 11:20:24 +0500

    using an array to map and render different topics

--- src/pages/Searchpage/TopicsList.tsx
+++ src/pages/Searchpage/TopicsList.tsx
@@ -1,3 +1,35 @@
+import { Box, Text } from "@chakra-ui/react";
+
 export default function TopicsList() {
-    return <div>TopicsList</div>;
+    const topics = [
+        "Philosophy",
+        "Literature",
+        "Web 3.0",
+        "Machine learning",
+        "Cyber security",
+        "Dark nature",
+        "Human psychology",
+        "Newtonian Physics",
+        "Astronomy",
+        "Fullstack development",
+    ];
+    return (
+        <Box display="flex" flexWrap="wrap" gap="2">
+            {topics.map((t) => {
+                return (
+                    <Text
+                        rounded="full"
+                        bg="gray.100"
+                        px="5"
+                        py="2"
+                        color="gray.600"
+                        fontSize="sm"
+                        fontWeight={"medium"}
+                    >
+                        {t}
+                    </Text>
+                );
+            })}
+        </Box>
+    );
 }
