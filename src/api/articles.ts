74ccf66a6b61969b8326349313541a0584554c97
mrx7355608 <mrx7355608@gmail.com>
2024-03-28 02:07:18 +0500

    uploading thumbnails to cloudinary

--- src/api/articles.ts
+++ src/api/articles.ts
@@ -12,3 +12,16 @@ export async function createArticle(article: IArticle) {
     const response = await axiosAgent.post("/api/articles", article);
     return response.data;
 }
+
+export async function uploadThumbnailToCloudinary(file: File) {
+    const cloudName = import.meta.env.VITE_CLOUDNAME;
+    const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET;
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", uploadPreset);
+
+    const cloudinaryURL = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+    const response = await axiosAgent.post(cloudinaryURL, formData);
+    console.log(response);
+}
--- src/pages/Write/ThumbnailSelector.tsx
+++ src/pages/Write/ThumbnailSelector.tsx
@@ -1,25 +1,56 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Input, Image } from "@chakra-ui/react";
+import { useState, useRef } from "react";
 import { FaCamera } from "react-icons/fa";
 
 export default function ThumbnailSelector() {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const [previewURL, setPreviewURL] = useState("");
     return (
-        <Box w="full" p="8" rounded="lg" bg="gray.50" mt="12">
-            <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                rounded="full"
-                w="60px"
-                h="60px"
-                borderColor="gray.600"
-                border="1px"
-                mx="auto"
-            >
-                <FaCamera size="20" />
+        <Box w="full" mt="12">
+            <Box w="full" p="8" rounded="lg" bg="gray.50" mb="6">
+                <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    rounded="full"
+                    w="60px"
+                    h="60px"
+                    borderColor="gray.600"
+                    border="1px"
+                    mx="auto"
+                    cursor="pointer"
+                    onClick={onClickHandler}
+                >
+                    <FaCamera size="20" />
+                </Box>
+                <Text textAlign="center" mt="4">
+                    Select a thumbnail
+                </Text>
+                <Input
+                    type="file"
+                    display="none"
+                    ref={fileInputRef}
+                    onChange={onChangeHandler}
+                />
             </Box>
-            <Text textAlign="center" mt="4">
-                Select a thumbnail
-            </Text>
+            {previewURL ? (
+                <Image
+                    objectFit={"cover"}
+                    w="full"
+                    src={previewURL}
+                    rounded="lg"
+                />
+            ) : null}
         </Box>
     );
+
+    function onClickHandler() {
+        fileInputRef.current?.click();
+    }
+
+    function onChangeHandler() {
+        const file = fileInputRef.current?.files![0];
+        const url = URL.createObjectURL(file!);
+        setPreviewURL(url);
+    }
 }
