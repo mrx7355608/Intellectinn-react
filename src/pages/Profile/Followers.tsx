6f95f3ae3340ac8dadcb9ecd511b08720f3eb845
mrx7355608 <mrx7355608@gmail.com>
2024-04-03 21:45:27 +0500

    created a separate user component without follow/unfollow btn

--- src/pages/Profile/Followers.tsx
+++ src/pages/Profile/Followers.tsx
@@ -1,9 +1,7 @@
-import React from "react";
 import useFetch from "../../hooks/useFetch";
 import { IUser } from "../../types/user";
 import { useAuthContext } from "../../context/auth";
-import { Spinner, Text, Box } from "@chakra-ui/react";
-import UserBox from "../../components/UserBoxComponent";
+import { Spinner, Text, Box, Image } from "@chakra-ui/react";
 
 export default function Followers() {
     const { user } = useAuthContext();
@@ -21,11 +19,51 @@ export default function Followers() {
                 <Text color="red.600">{err}</Text>
             ) : following.length > 0 ? (
                 following.map((user) => {
-                    return <UserBox user={user} />;
+                    return <User user={user} />;
                 })
             ) : (
                 <Text>Nothing to show</Text>
             )}
+        </Box>
+    );
+}
+
+function User({ user }: { user: IUser }) {
+    return (
+        <Box
+            mb="10"
+            display="flex"
+            alignItems="start"
+            justifyContent="space-between"
+            gap="4"
+            w="full"
+        >
+            <Box display="flex">
+                {/* user profile picture */}
+                <Image
+                    w="60px"
+                    h="60px"
+                    src={user.profilePicture}
+                    rounded="full"
+                    objectFit={"cover"}
+                />
+
+                {/* user fullname and about */}
+                <Box ml="4">
+                    <Text fontWeight={"bold"} fontSize="lg">
+                        {user.fullname}
+                    </Text>
+                    {user.about ? (
+                        <Text color="gray.500" mr="6">
+                            {user.about.substring(0, 60)}...
+                        </Text>
+                    ) : (
+                        <Text fontStyle={"italic"} color="gray.500">
+                            No about content provided
+                        </Text>
+                    )}
+                </Box>
+            </Box>
         </Box>
     );
 }
