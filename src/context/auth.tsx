cb51ca7baf3045db565981d941ea995dc9f0e906
mrx7355608 <mrx7355608@gmail.com>
2024-10-02 12:59:34 +0500

    created utility functions to help in user state updates

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -5,10 +5,65 @@ type Store = {
     user: IUser | null;
     loginUser: (user: IUser) => void;
     logoutUser: () => void;
+    updateAbout: (about: string) => void;
+    updateProfilePicture: (profilePicture: string) => void;
+    addFollower: (followerID: string) => void;
+    removeFollower: (followerID: string) => void;
+    addFollowing: (followingID: string) => void;
+    removeFollowing: (followingID: string) => void;
+};
+
+const updateUserHelper = (state: Store, updates: Partial<IUser>) => {
+    if (!state.user) {
+        return state.user;
+    }
+
+    return { ...state.user, ...updates };
 };
 
 export const useAuth = create<Store>()((set) => ({
     user: null,
-    loginUser: (user) => set(() => ({ user: user })),
+    loginUser: (user) => set(() => ({ user })),
     logoutUser: () => set(() => ({ user: null })),
+    updateAbout: (about) =>
+        set((state) => ({
+            user: updateUserHelper(state, { about }),
+        })),
+
+    updateProfilePicture: (profilePicture) =>
+        set((state) => ({
+            user: updateUserHelper(state, { profilePicture }),
+        })),
+
+    addFollower: (followerID) =>
+        set((state) => ({
+            user: updateUserHelper(state, {
+                followers: [...(state.user?.followers || []), followerID],
+            }),
+        })),
+
+    removeFollower: (followerID) =>
+        set((state) => ({
+            user: updateUserHelper(state, {
+                followers: state.user?.followers.filter(
+                    (id) => id !== followerID,
+                ),
+            }),
+        })),
+
+    addFollowing: (followingID) =>
+        set((state) => ({
+            user: updateUserHelper(state, {
+                following: [...(state.user?.following || []), followingID],
+            }),
+        })),
+
+    removeFollowing: (followingID) =>
+        set((state) => ({
+            user: updateUserHelper(state, {
+                followers: state.user?.following.filter(
+                    (id) => id !== followingID,
+                ),
+            }),
+        })),
 }));
