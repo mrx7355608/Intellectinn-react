d98bac4e069bb4f5de08588ff6ad14e1ecb5b6f4
mrx7355608 <mrx7355608@gmail.com>
2024-10-04 12:28:30 +0500

    updated NotFound page styles

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,18 @@
-import {Container, Heading, Text} from "@chakra-ui/react"
-import { Link } from "react-router-dom"
+import { VStack, Heading, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
-export default function NotFound(){
+export default function NotFound() {
     return (
-        <Container textAlign={"center"}>
-            <Heading fontSize={"4xl"}>404 Not Found</Heading>
-            <Text color="gray.600" mt={2} mb={5}>The page you are trying to access does not exist</Text>
+        <VStack justifyContent={"center"} minH={"100vh"}>
+            <Heading fontSize={"5xl"}>404 Not Found</Heading>
+            <Text color="gray.600" mt={2} mb={5} fontSize={"lg"}>
+                The page you are trying to access does not exist
+            </Text>
             <Link to="/">
                 <Text textDecoration={"underline"} color="blue.500">
                     Go to homepage
                 </Text>
             </Link>
-        </Container>
-    )
+        </VStack>
+    );
 }
